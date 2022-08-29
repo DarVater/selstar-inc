99dc9bd8eca4f898e4333ec56e64e87f4f9b1a0a
bilcko <iubilcko@gmail.com>
2022-08-29 11:29:10 +0300

    create connect to mongoDB

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const config = require('config')
-
+const mongoose = require("mongoose");
 
 const app = express()
 
 const PORT = config.get('port') || 5000
 
-app.listen(PORT, () => {
-    console.log(`App has been started at ${PORT}...`)
-})
+async function start() {
+    try {
+        console.log(mongoose.connect)
+        await mongoose.connect(config.get('mongoUri'),
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        )
+        app.listen(PORT, () => {
+            console.log(`App has been started at ${PORT}...`)
+        })
+    } catch (e) {
+        console.log("Server error", e.message)
+        process.exit(1)
+    }
+}
+start()
